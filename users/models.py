3d4aa46fb83da1dc32c5c8f21193b4464761252d
Patrick <kimanthip@gmail.com>
2021-09-17 21:28:24 +0300

    add profile and skill models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
+from django.db.model.signals import post_save, post_delete
+from django.dispatch import receiver
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    phone_number = models.CharField(max_length=200, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(
+        blank=True, null=True, upload_to='profiles/', default='profiles/user-default.png')
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.username)
+
+    class Meta:
+        ordering = ['created']
+
+    @property
+    def imageUrl(self):
+        try:
+            url = self.profile_image.url
+        except:
+            url = ''
+        return url
+class Skill(models.Model):
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
