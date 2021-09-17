aae1e705d27de21213fef0a582f649b9d2594015
Patrick <kimanthip@gmail.com>
2021-09-17 23:01:03 +0300

    add profiles function

--- users/views.py
+++ users/views.py
@@ -82,3 +82,17 @@ def editAccount(request):
     }
     return render(request, 'users/profile-form.html', context)
 
+
+def profiles(request):
+    profiles, search_query = searchProfiles(request)
+
+    custom_range, profiles = paginateProfiles(request, profiles, 3)
+
+    context = {
+        'profiles': profiles,
+        'search_query': search_query,
+        'custom_range': custom_range
+    }
+    return render(request, 'users/profiles.html', context)
+
+
