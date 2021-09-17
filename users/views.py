504a0cd1a768b41c13e8cf242f7ccd0bf1d99d7c
Patrick <kimanthip@gmail.com>
2021-09-17 21:32:19 +0300

    add login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Profile
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+# from .utils import searchProfiles, paginateProfiles
 # Create your views here.
+
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
+        else:
+            messages.error(request, 'Username or password in incorrect')
+
+    return render(request, 'users/login_register.html')
