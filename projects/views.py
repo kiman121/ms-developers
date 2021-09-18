de0aa6ae916c81b0ac3d2aa1ad3a8cfe3c37df3d
Patrick <kimanthip@gmail.com>
2021-09-18 08:47:13 +0300

    add projects view function

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Project
+from .forms import ProjectForm, ReviewForm
+from .utils import searchProjects, paginateProjects
 
-# Create your views here.
+
+def projects(request):
+
+    projects, search_query = searchProjects(request)
+
+    custom_range, projects = paginateProjects(request, projects, 6)
+
+    context = {
+        'projects': projects,
+        'search_query': search_query,
+        'custom_range': custom_range
+    }
+
+    return render(request, 'projects/projects.html', context)
