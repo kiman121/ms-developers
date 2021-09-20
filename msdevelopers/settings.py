deb7f4bf1b2133c8f9742a6d72c882e669e18456
Patrick <kimanthip@gmail.com>
2021-09-21 02:41:24 +0300

    Deployment 2

--- msdevelopers/settings.py
+++ msdevelopers/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from datetime import timedelta
+from datetime import timedelta 
+
 from pathlib import Path
 import django_heroku
 import dj_database_url
