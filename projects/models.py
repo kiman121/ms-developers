1bf25ac44084600f70b8de6e2bfc8a2c62e01f80
Patrick <kimanthip@gmail.com>
2021-09-21 02:15:15 +0300

    refinement on the projects models

--- projects/models.py
+++ projects/models.py
@@ -13,6 +13,9 @@ class Project(models.Model):
     featured_image = models.ImageField(
         null=True, blank=True, default='default.jpg')
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    design_score = models.IntegerField(default=0, null=True, blank=True)
+    usability_score = models.IntegerField(default=0, null=True, blank=True)
+    content_score = models.IntegerField(default=0, null=True, blank=True)
     rating = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
@@ -40,27 +43,29 @@ class Project(models.Model):
     @property
     def getRating(self):
         reviews = self.review_set.all()
-        designRating = reviews.aggregate(Sum(design))
-        usabilityRating = reviews.aggregate(Sum(usability))
-        contentRating = reviews.aggregate(Sum(content))
 
-        score = (designRating + usabilityRating + contentRating)/3
+        design_score = 0
+        usability_score = 0
+        content_score = 0
+        total_score = 0
+
+        for review in reviews:
+            self.design_score += review.design
+            self.usability_score += review.usability
+            self.content_score += review.content
+
+            total_score += (review.design+review.usability+review.content)/3
         
-        # upVotes = reviews.filter(value='up').count()
-        # totalVotes = reviews.count()
-        
-        # ratio = (upVotes/totalVotes) * 100
-        # self.vote_total = totalVotes
-        self.rating = round(score)
+        self.rating = round(total_score)
         
         self.save()
 
 class Review(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    design = models.IntegerField(choices=list(zip(range(1, 10), range(1, 10))), unique=True)
-    usability = models.IntegerField(choices=list(zip(range(1, 10), range(1, 10))), unique=True)
-    content = models.IntegerField(choices=list(zip(range(1, 10), range(1, 10))), unique=True)
+    design = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))), unique=True)
+    usability = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))), unique=True)
+    content = models.IntegerField(choices=list(zip(range(1, 11), range(1, 11))), unique=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
@@ -69,4 +74,5 @@ class Review(models.Model):
         unique_together = [['owner', 'project']]
 
     def __str__(self):
-        return self.value
+        return self.project.title
+
